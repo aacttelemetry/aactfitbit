521be35ae9aedf5a5cc98dd043fac0779026a2fc
lgactna <48928674+lgactna@users.noreply.github.com>
2020-07-16 15:20:13 -0700

    varied changes
    from last commit:
    - added extra ui elements for testing
    - changed css for easier debug
    - added app icon
    - changed internal websocket address
    - implemented intial identification message

--- app/index.js
+++ app/index.js
@@ -34,11 +34,18 @@ const orientationData = document.getElementById("orientation-data");
 const incomingLabel = document.getElementById("incoming-label");
 const incomingData = document.getElementById("incoming-data");
 
+//https://dev.fitbit.com/build/guides/user-interface/svg-components/buttons/
+let mybutton2 = document.getElementById("mybutton2");
+mybutton2.onactivate = function(evt) {
+  console.log("CLICKED!");
+}
+
+
 const sensors = [];
 
-var heart_last;
-var body_presence_last;
-var barometer_last;
+var heart_last = 0;
+var body_presence_last = false;
+var barometer_last = 0;
 var incoming;
 
 //if (Accelerometer) -> "if accelerometer sensor is present"
@@ -64,6 +71,7 @@ if (Barometer) {
     barData.text = JSON.stringify({
       pressure: barometer.pressure ? parseInt(barometer.pressure) : 0
     });
+    barometer_last = barometer.pressure ? parseInt(barometer.pressure) : 0
   });
   sensors.push(barometer);
   barometer.start();
@@ -78,6 +86,7 @@ if (BodyPresenceSensor) {
     bpsData.text = JSON.stringify({
       presence: bps.present
     })
+    body_presence_last = bps.present
   });
   sensors.push(bps);
   bps.start();
@@ -110,7 +119,11 @@ if (HeartRateSensor) {
     }); //Converts objects inside this array into a string object.
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
     //The below will just directly throw it to an array.
-    heart_last = hrm.heartRate ? hrm.heartRate : 0
+    if(body_presence_last == false || !(hrm.heartRate)){
+      heart_last = 0;
+    }else{
+      heart_last = hrm.heartRate;
+    }
   });
   sensors.push(hrm);
   hrm.start();
@@ -133,11 +146,13 @@ if (OrientationSensor) {
   orientationData.style.display = "none";
 }
 
+/*
+//Comment this out on test runs
 display.addEventListener("change", () => {
   // Automatically stop all sensors when the screen is off to conserve battery
   display.on ? sensors.map(sensor => sensor.start()) : sensors.map(sensor => sensor.stop());
 });
-
+*/
 ///////////////////////////////////////////////////////
 
 messaging.peerSocket.onopen = function() {
@@ -151,7 +166,7 @@ messaging.peerSocket.onerror = function(err) {
   console.log("Connection error: " + err.code + " - " + err.message);
 }
 
-//Listen for stuff from the app (from the websocket)
+//Listen for stuff from the app (from the websocket) and update the text accordingly
 messaging.peerSocket.onmessage = function(evt) {
   incomingData.text = JSON.stringify(evt.data)
 }
@@ -160,7 +175,8 @@ setInterval(function(){
   var to_companion = {
     timestamp: Math.floor(Date.now()/1000),
     hr_arr: hrmData.text,
-    hr_only: heart_last
+    hr_only: heart_last,
+    pressure: barometer_last
   }
   if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
     // Send the data to peer as a message
--- companion/index.js
+++ companion/index.js
@@ -1,8 +1,10 @@
 // Import the messaging module
 import * as messaging from "messaging";
 
-const wsUri = "ws://192.168.1.72:12345";
-const websocket = new WebSocket(wsUri);
+const wsUri = "ws://192.168.1.85:8765";
+var websocket = WebSocket(wsUri);
+var temp = [];
+var init_send = false;
 /*
 while(true){
   if(websocket){
@@ -13,29 +15,49 @@ while(true){
 }
 */
 setTimeout(1000)
-// Listen for the onmessage event
+// Listen for the onmessage event (coming from the Fitbit)
 messaging.peerSocket.onmessage = function(evt) {
   // Output the message to the console
   console.log(JSON.stringify(evt.data));
-  websocket.send(JSON.stringify(evt.data));
+  /*
+  temp.push(evt.data);
+  // And send to the client, but only if it's there; else, just hold the queue
+  if (websocket.readyState==1){
+    websocket.send(JSON.stringify(temp));
+    temp = [];
+  }
+  */
+  //wait until identifying message has been sent
+  if (init_send == true){
+    websocket.send(JSON.stringify(evt.data));//don't use queue
+  }
+  
 }
 
 websocket.onopen = function(event) {
   console.log("WebSocket is open now.");
+  websocket.send("fitbit");//theoretically this should always be the first message sent
+  init_send = true;
 };
 
 websocket.onclose = function(event) {
   console.log("WebSocket is closed now.");
+  init_send = false;
 };
 
 websocket.addEventListener("message", onMessage);
 function onMessage(evt) {
    var received = evt.data
    console.log("got: "+received);
-   messaging.peerSocket.send(received);
+   messaging.peerSocket.send(received);//Send data to actual watch
 }
 
-
+//check and print the current state of the websocket
+//since it is typically set correctly (not stuck in 0 or 2), there is no need to change the port and target
+//0,1,2,3 - connecting, open, closing, closed
 setInterval(function(){
   console.log(websocket.readyState);
+  if(websocket.readyState == 3){
+    websocket = WebSocket(wsUri) //automatically attempt to reconnect by recreating the socket
+  }//note that the rpi is such that it will automatically reconnect, so long as it's running
 },1000);
